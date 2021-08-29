0cbc45b2e579d2c0dee1dd19fef24973c651f813
Robson Cristian de Lira <robso.crlira@gmail.com>
2021-08-29 15:41:51 -0300

    Deixando as mensagens de erro mais dinamicas

--- src/app/shared/components/campos/validar-campos.service.ts
+++ src/app/shared/components/campos/validar-campos.service.ts
@@ -6,6 +6,7 @@ import { AbstractControl } from "@angular/forms";
 })
 export class ValidarCamposService {
   constructor() {}
+
   hasErrorValidate(control: AbstractControl, errorName: string): boolean {
     if (
       (control.dirty || control.touched) &&
@@ -15,7 +16,13 @@ export class ValidarCamposService {
     }
     return false;
   }
+
   hasError(control: AbstractControl, errorName: string): boolean {
     return control.hasError(errorName);
+  }
+
+  lengthValidate(control: AbstractControl, errorName: string): number {
+    const error = control.errors[errorName];
+    return error.requiredLength || error.min || error.max || 0;
   }
 }
